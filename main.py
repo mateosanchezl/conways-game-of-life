536f30cc35b29000d0e13f52fe6f867dd69439fc
mateo <mateosanchez.msl@gmail.com>
2024-03-29 12:59:01 +0000

    v1

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import numpy as np
 # Any live cell with more than three live neighbours dies, as if by overpopulation.
 
 # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+
 class Button:
     def __init__(self, x, y, w, h, text, screen: pygame.surface.Surface, func):
         self.x = x
@@ -16,32 +17,26 @@ class Button:
         self.w = w
         self.h = h
         self.text = text
-        self.color_light = (200, 200, 200)
-        self.color_dark = (144, 144, 144)
+        self.COLOUR_LIGHT = (106, 90, 205)
+        self.COLOUR_DARK = (177, 13, 201) 
         self.screen = screen
-        self.font = pygame.font.SysFont('Corbel', 35)
-        self.button_text = self.font.render(self.text, True, 'black')
-        self.button_rect = pygame.Rect(x, y, w, h)
+        self.font = pygame.font.SysFont('Arial', 20)
+        self.button_text = self.font.render(self.text, True, (255, 255, 255))
         self.func = func
-    
-    def hovering(self, mouse_x, mouse_y):
-        return self.x < mouse_x < self.x + self.w and self.y < mouse_y < self.y + self.h
-    
-    def display(self, mouse_x, mouse_y):
-        
-        if self.hovering(mouse_x, mouse_y):
-            pygame.draw.rect(self.screen, self.color_dark, self.button_rect)
-        else:
-            pygame.draw.rect(self.screen, self.color_light, self.button_rect)
-        self.screen.blit(self.button_text, (self.x, self.y))
-        
-    # TO-DO
-    # 1. Make button class
-    # 2. Specify which settings of the game to change - restart, more or less cells, speed of simulation, pausing, starting
-    # 3. Implement these settings so they can be changed live
-    # 4. Allow buttons to change different settings of the game
-    
+        self.text_rect = self.button_text.get_rect(center=(x + w // 2, y + h // 2))
+        self.button_rect = pygame.Rect(x, y, w, h)
 
+    def hovering(self, mouse_x, mouse_y):
+        return self.button_rect.collidepoint(mouse_x, mouse_y)
+
+    def draw_rounded_rect(self, rect, color, radius=0.4):
+        corner_radius = int(radius * min(rect.size))
+        pygame.draw.rect(self.screen, color, rect, border_radius=corner_radius)
+
+    def display(self, mouse_x, mouse_y):
+        color = self.COLOUR_DARK if self.hovering(mouse_x, mouse_y) else self.COLOUR_LIGHT
+        self.draw_rounded_rect(self.button_rect, color)
+        self.screen.blit(self.button_text, self.text_rect)
     
 class Game:
     pygame.init()
@@ -51,8 +46,8 @@ class Game:
         self.WIDTH = self.screen.get_width()
         self.HEIGHT = self.screen.get_height()
         self.BLOCK_SIZE = 5
-        self.BG_COLOUR = (8, 0, 66, 1)
-        self.CELL_COLOUR = 251, 0, 130, 0.76
+        self.BG_COLOUR = (11, 38, 40)
+        self.CELL_COLOUR = (57, 255, 20)
         self.FRAME_RATE = 60
         self.LIFE_COUNT = 500
         self.grid = np.zeros(shape=(int(self.SCREEN_SIZE[0] / self.BLOCK_SIZE), int(self.SCREEN_SIZE[1] / self.BLOCK_SIZE)))
@@ -61,10 +56,10 @@ class Game:
         self.play = False
         self.font = pygame.font.SysFont('Arial', 40)
         self.generated_cell_coords = []
-        self.buttons = [Button(self.WIDTH / 2 - 40,10, 80, 40,'Start', screen=self.screen, func=self.toggle_play),
-                        Button(self.WIDTH / 2 + 80, 10, 80, 40, 'Reset', screen=self.screen, func=self.reset_grid),
-                        Button(self.WIDTH / 2 - 160, 10, 80, 40, '+', screen=self.screen, func=self.create_life),
-                        Button(self.WIDTH / 2 - 250, 10, 80, 40, '-', screen=self.screen, func=self.destroy_life)]
+        self.buttons = [Button(self.WIDTH / 2 - 40,10, 110, 40,'Play/Pause', screen=self.screen, func=self.toggle_play),
+                        Button(self.WIDTH / 2 + 80, 10, 60, 40, 'Reset', screen=self.screen, func=self.reset_grid),
+                        Button(self.WIDTH / 2 - 90, 10, 40, 40, '+', screen=self.screen, func=self.create_life),
+                        Button(self.WIDTH / 2 - 140, 10, 40, 40, '-', screen=self.screen, func=self.destroy_life)]
 
     def check_live_neighbours(self, row, col):
         rows, cols = self.grid.shape
@@ -126,13 +121,11 @@ class Game:
                         if button.hovering(mouse_x, mouse_y):
                             button.func()                        
                      
-            # fill the screen with a color to wipe away anything from last frame
             self.screen.fill(self.BG_COLOUR)
             
             if self.play: 
                 self.step()
               
-            # RENDER YOUR GAME HERE
             for i in range(len(self.grid[:, 0])):
                 for j in range(len(self.grid[0])):
                     x = i * self.BLOCK_SIZE
@@ -140,21 +133,16 @@ class Game:
                     cell = pygame.Rect(x, y, self.BLOCK_SIZE, self.BLOCK_SIZE)
                     pygame.draw.rect(self.screen, self.CELL_COLOUR if self.alive(self.grid[i][j], self.check_live_neighbours(i, j)) else self.BG_COLOUR, width=5, rect=cell)
                     
-            # Play button
             for button in self.buttons:
                 button.display(mouse_x, mouse_y)
-            # flip() the display to put your work on screen
                     
             pygame.display.flip()
 
             self.clock.tick(self.FRAME_RATE) 
 
         pygame.quit()
-
-
-
-    
+        
+           
 if __name__ == "__main__":
     game = Game()
     game.run()
-    
